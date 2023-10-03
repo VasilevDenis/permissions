38dc072bbd43b2700c7ab1a12e81612195a0be03
Denis Vasilev <pythocoder@gmail.com>
2023-10-03 21:56:17 +0600

    'on_the_middle'

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,12 +1,23 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
+
+from advertisements.permissions import IsOwner
+from advertisements.models import Advertisement
+from advertisements.serializers import AdvertisementSerializer
 
 
 class AdvertisementViewSet(ModelViewSet):
-    """ViewSet для объявлений."""
+    """ViewSet для объявлений!"""
 
     # TODO: настройте ViewSet, укажите атрибуты для кверисета,
     #   сериализаторов и фильтров
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    permission_classes = [IsAuthenticated, IsOwner]
+    # permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
     def get_permissions(self):
         """Получение прав для действий."""
